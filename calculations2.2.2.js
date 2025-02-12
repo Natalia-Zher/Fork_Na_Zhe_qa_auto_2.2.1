37f5d829eeee9e5b0c1486c2e1825f38b4487b94
Natalia Zherebtcova <natalya.zherebtsova@gmail.com>
2025-02-12 14:55:28 +0100

    added increment and decrement in the functions in the code 2.2.4

--- calculations2.2.2.js
+++ calculations2.2.2.js
@@ -1,24 +1,27 @@
 function increaseBalance(x, y) {
     x += y;
-    return x;
+    return ++x; // инкрементируем перед возвратом
   }
   
   function decreaseBalance(x, y) {
-  x -= y;
-  return x;
+  x -= y;   
+  return --x; // декрементируем перед возвратом
   }
   
   function divideBalanceByAccounts(x, y) {
     x /= y;
-    return x;
-    console.log("x");
+    return ++x; // инкрементируем перед возвратом
   }
   
   function getRestAfterDivision(x, y) {
     x %= y;
     return x;
+    // Оператор %= выполняет операцию взятия остатка от деления и присваивает результат переменной. 
+    // Однако его использование внутри increaseBalance и decreaseBalance не имеет смысла, 
+    // так как остаток от деления не связан с увеличением или уменьшением баланса.
   }
   
+
   console.log(increaseBalance(3000, 700));
   console.log(decreaseBalance(3000, 700));
   console.log(divideBalanceByAccounts(3000, 2));
